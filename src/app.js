4a538d5f361c550a44477d32330e38a4158b9789
Adam Karlsten <adamkarlsten@gmail.com>
2018-03-21 02:01:37 +0100

    configured webpack for production

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense, editExpense, removeExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import moment from 'moment'
@@ -15,11 +12,8 @@ import 'react-dates/lib/css/_datepicker.css'
 moment.locale(navigator.language)
 
 const store = configureStore()
-store.dispatch(addExpense({ description: 'Water bill', amount: 5000, createdAt: 10 }))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 80000, createdAt: 3000 }))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 80000, createdAt: 3100 }))
-store.dispatch(addExpense({ description: 'rent', amount: 3000, createdAt: 3600 }))
 
+console.log('hi there')
 const jsx = (
   <Provider store={store}>
     <AppRouter />
