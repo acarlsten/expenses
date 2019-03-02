aa5db7bfd9b24a8c9661d7346b04ec810b05692c
Adam Karlsten <adamkarlsten@gmail.com>
2019-03-02 15:40:37 +0100

    added and configured numeral.js to format numbers

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import numeral from 'numeral'
+import 'numeral/locales/no'
+numeral.locale('no') // same rules as SE
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>Summa: {(amount / 100).toLocaleString()} kr</p>
+    <p>Summa: {numeral(amount / 100).format('0,0[.]00 $')}</p>
     <p>Skapad: {moment(createdAt).format('LL')}</p>
   </div>
 )
