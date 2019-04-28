64eb53fc4f861c8a073735929096c842150d994b
Adam Karlsten <adamkarlsten@gmail.com>
2019-04-29 00:46:33 +0200

    added asynchronous redux actions for adding to and reading from firebase with tests

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -2,23 +2,25 @@ const expensesReducerDefaultState = []
 
 export default (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
-  case 'ADD_EXPENSE':
-    return [...state, action.expense]
-  case 'REMOVE_EXPENSE':
-    return state.filter(({ id }) => action.id !== id)
-  case 'EDIT_EXPENSE':
-    return state.map(expense => {
-      // trying to change id causes silent failure, for now
-      if (expense.id === action.id && !action.updates.id) {
-        return {
-          ...expense,
-          ...action.updates
+    case 'ADD_EXPENSE':
+      return [...state, action.expense]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => action.id !== id)
+    case 'EDIT_EXPENSE':
+      return state.map(expense => {
+        // trying to change id causes silent failure, for now
+        if (expense.id === action.id && !action.updates.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense
         }
-      } else {
-        return expense
-      }
-    })
-  default:
-    return state
+      })
+    case 'SET_EXPENSES':
+      return action.expenses
+    default:
+      return state
   }
 }
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -85,3 +85,14 @@ test('shouldnt edit an expense with invalid id', () => {
 
   expect(state).toEqual(expenses)
 })
+
+test('should set expenses', () => {
+  const action = {
+    type: 'SET_EXPENSES',
+    expenses: [expenses[1]]
+  }
+
+  const state = expensesReducer(expenses, action)
+
+  expect(state).toEqual([expenses[1]])
+})
