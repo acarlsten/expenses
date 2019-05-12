549c7cc1e757e3beb4a9ce5152bda420512d6c6b
Adam Karlsten <adamkarlsten@gmail.com>
2019-05-13 00:36:24 +0200

    finished authentication, begun styling

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import selectExpenses from '../selectors/expenses'
 import getExpensesTotal from '../selectors/expenses-total'
 import numeral from 'numeral'
@@ -7,15 +8,23 @@ import 'numeral/locales/no'
 numeral.locale('no') // same rules as SE
 
 export const ExpensesSummary = props => (
-  <div>
-    {props.expensesCount === 0 ? (
-      <p>Nothing to see here!</p>
-    ) : (
-      <p>
-        Viewing {props.expensesCount} {props.expensesCount === 1 ? 'expense' : 'expenses'} totalling{' '}
-        {numeral(props.expensesTotal / 100).format('0,0[.]00 $')}
-      </p>
-    )}
+  <div className="page-header">
+    <div className="content-container">
+      {props.expensesCount === 0 ? (
+        <h1>No expenses, add one!</h1>
+      ) : (
+        <h1 className="page-header__title">
+          Viewing <span>{props.expensesCount}</span>{' '}
+          {props.expensesCount === 1 ? 'expense' : 'expenses'} totalling{' '}
+          <span>{numeral(props.expensesTotal / 100).format('0,0[.]00 $')}</span>
+        </h1>
+      )}
+      <div className="page-header__actions">
+        <Link className="button" to="/create">
+          Add Expense
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
