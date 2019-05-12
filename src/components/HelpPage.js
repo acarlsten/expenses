459d02d75d39091b84d13f4d2aa4ef4364c8aec8
Adam Karlsten <adamkarlsten@gmail.com>
2019-05-12 13:57:09 +0200

    working on authentication

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const HelpPage = () => <div>This is the help page</div>
+const HelpPage = () => (
+  <div>
+    <p>This is the help page</p>
+  </div>
+)
 
 export default HelpPage
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,20 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 import toJSON from 'enzyme-to-json'
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
+
+let startLogout, wrapper
+
+beforeEach(() => {
+  startLogout = jest.fn()
+  wrapper = shallow(<Header startLogout={startLogout} />)
+})
 
 test('should render header correctly', () => {
-  const wrapper = shallow(<Header />)
   expect(toJSON(wrapper)).toMatchSnapshot()
+})
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').simulate('click')
+  expect(startLogout).toHaveBeenCalled()
 })
